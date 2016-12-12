c8210fef118fe12e0f9a284edcf69ba2419b2e08
ubbeK <urban.nyden@gmail.com>
2016-12-12 15:33:19 +0100

    Removing unused SQLiteMain method

--- src/StarDump/CommandInterface.cs
+++ src/StarDump/CommandInterface.cs
@@ -67,51 +67,5 @@ namespace StarDump
 
             return true;
         }
-
-        public static void SqliteMain(string[] args)
-        {
-            string sqliteFileRoot = "D:/Temp/Core/Database";
-            string sqliteFileName = "MySQLiteDatabase.sqlite3";
-            FileInfo fi = new FileInfo(Path.Combine(sqliteFileRoot, sqliteFileName));
-
-            if (!fi.Directory.Exists)
-            {
-                fi.Directory.Create();
-            }
-
-            string connectionString = string.Format("Data Source={0}", fi.FullName);
-            SqliteConnection cn = new SqliteConnection(connectionString);
-            SqliteCommand cmd = null;
-
-            cn.Open();
-            Console.WriteLine("Connection open: " + fi.FullName);
-
-            cmd = new SqliteCommand("CREATE TABLE IF NOT EXISTS `Item` (`ObjectNo` INTEGER NOT NULL, `Name` TEXT NOT NULL, PRIMARY KEY(`ObjectNo`));", cn);
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Table 'Item' created");
-
-            long? max;
-
-            cmd = new SqliteCommand("SELECT MAX(ObjectNo) FROM Item", cn);
-            max = cmd.ExecuteScalar() as long?;
-
-            if (max == null)
-            {
-                max = 0;
-            }
-            else
-            {
-                max++;
-            }
-
-            cmd = new SqliteCommand("INSERT INTO `Item` (ObjectNo, Name) VALUES (@ObjectNo, @Name);", cn);
-            cmd.Parameters.Add(new SqliteParameter() { ParameterName = "@ObjectNo", Value = max });
-            cmd.Parameters.Add(new SqliteParameter() { ParameterName = "@Name", Value = "Item " + max });
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Item {0} inserted", max);
-
-            cn.Close();
-            Console.WriteLine("Connection closed");
-        }
     }
 }
