91d7244086e9a7957fedbd6780322fccfdb5a55a
Konstantin <const.miyang@gmail.com>
2016-12-14 12:59:37 +0100

    Move GetValue method from UnloadRow to CrudHelper

--- src/StarDump/Helpers/CrudHelper.cs
+++ src/StarDump/Helpers/CrudHelper.cs
@@ -10,6 +10,7 @@ namespace StarDump
     {
         public string TableName { get; protected set; }
         protected Dictionary<string, ulong> getters = new Dictionary<string, ulong>();
+        protected Dictionary<string, Func<ulong, ulong, ulong, object>> methods = new Dictionary<string, Func<ulong, ulong, ulong, object>>();
 
         public CrudHelper(ulong dbHandle, UnloadTable table)
         {
@@ -21,12 +22,94 @@ namespace StarDump
 
                 sccrud.star_crud_GetPropertyReadHandle(dbHandle, this.TableName, c.Name, out getter);
                 getters.Add(c.Name, getter);
+                methods.Add(c.Name, this.GetMethod(c.DataTypeName, c.Nullable));
             }
         }
 
         public ulong GetGetter(string columnName)
         {
             return getters[columnName];
+        }
+
+        public object GetValue(ulong dbId, ulong dbRef, string columnName)
+        {
+            ulong getter = getters[columnName];
+            Func<ulong, ulong, ulong, object> method = this.methods[columnName];
+            object value = method(dbId, dbRef, getter);
+
+            return value;
+        }
+
+        protected Func<ulong, ulong, ulong, object> GetMethod(string dataTypeName, bool nullable)
+        {
+            if (nullable)
+            {
+                switch (dataTypeName)
+                {
+                    case "string": return (dbId, dbRef, getter) => DbCrud.GetString(dbId, dbRef, getter);
+                    case "byte[]": return (dbId, dbRef, getter) => DbCrud.GetBinary(dbId, dbRef, getter);
+                    case "bool": return (dbId, dbRef, getter) => DbCrud.GetNullableBool(dbId, dbRef, getter);
+                    case "byte": return (dbId, dbRef, getter) => DbCrud.GetNullableByte(dbId, dbRef, getter);
+                    case "char": return (dbId, dbRef, getter) => DbCrud.GetNullableChar(dbId, dbRef, getter);
+                    case "DateTime": return (dbId, dbRef, getter) => DbCrud.GetNullableDateTime(dbId, dbRef, getter);
+                    case "decimal": return (dbId, dbRef, getter) => DbCrud.GetNullableDecimal(dbId, dbRef, getter);
+                    case "double": return (dbId, dbRef, getter) => DbCrud.GetNullableDouble(dbId, dbRef, getter);
+                    case "float": return (dbId, dbRef, getter) => DbCrud.GetNullableFloat(dbId, dbRef, getter);
+                    case "int": return (dbId, dbRef, getter) => DbCrud.GetNullableInt(dbId, dbRef, getter);
+                    case "long": return (dbId, dbRef, getter) => DbCrud.GetNullableLong(dbId, dbRef, getter);
+                    case "sbyte": return (dbId, dbRef, getter) => DbCrud.GetNullableSByte(dbId, dbRef, getter);
+                    case "short": return (dbId, dbRef, getter) => DbCrud.GetNullableShort(dbId, dbRef, getter);
+                    case "uint": return (dbId, dbRef, getter) => DbCrud.GetNullableUInt(dbId, dbRef, getter);
+                    case "reference":
+                        return (dbId, dbRef, getter) => 
+                        {
+                            var m = DbCrud.GetDb<Starcounter.Internal.Metadata.MotherOfAllLayouts>(dbId, dbRef, getter);
+
+                            if (m == null)
+                            {
+                                return null;
+                            }
+
+                            return m.GetObjectNo();
+                        };
+                    case "ulong": return (dbId, dbRef, getter) => DbCrud.GetNullableULong(dbId, dbRef, getter);
+                    case "ushort": return (dbId, dbRef, getter) => DbCrud.GetNullableUShort(dbId, dbRef, getter);
+                    default: throw new NotImplementedException("The data type [" + dataTypeName + "] is not supported.");
+                }                
+            }
+
+            switch (dataTypeName)
+            {
+                case "bool": return (dbId, dbRef, getter) => DbCrud.GetBool(dbId, dbRef, getter);
+                case "byte": return (dbId, dbRef, getter) => DbCrud.GetByte(dbId, dbRef, getter);
+                case "char": return (dbId, dbRef, getter) => DbCrud.GetChar(dbId, dbRef, getter);
+                case "DateTime": return (dbId, dbRef, getter) => DbCrud.GetDateTime(dbId, dbRef, getter);
+                case "decimal": return (dbId, dbRef, getter) => DbCrud.GetDecimal(dbId, dbRef, getter);
+                case "double": return (dbId, dbRef, getter) => DbCrud.GetDouble(dbId, dbRef, getter);
+                case "float": return (dbId, dbRef, getter) => DbCrud.GetFloat(dbId, dbRef, getter);
+                case "int": return (dbId, dbRef, getter) => DbCrud.GetInt(dbId, dbRef, getter);
+                case "long": return (dbId, dbRef, getter) => DbCrud.GetLong(dbId, dbRef, getter);
+                case "sbyte": return (dbId, dbRef, getter) => DbCrud.GetSByte(dbId, dbRef, getter);
+                case "short": return (dbId, dbRef, getter) => DbCrud.GetShort(dbId, dbRef, getter);
+                case "string": return (dbId, dbRef, getter) => DbCrud.GetString(dbId, dbRef, getter);
+                case "uint": return (dbId, dbRef, getter) => DbCrud.GetUInt(dbId, dbRef, getter);
+                case "ulong": return (dbId, dbRef, getter) => DbCrud.GetULong(dbId, dbRef, getter);
+                case "ushort": return (dbId, dbRef, getter) => DbCrud.GetUShort(dbId, dbRef, getter);
+                case "byte[]": return (dbId, dbRef, getter) => DbCrud.GetBinary(dbId, dbRef, getter);
+                case "reference":
+                    return (dbId, dbRef, getter) => 
+                    {
+                        var m = DbCrud.GetDb<Starcounter.Internal.Metadata.MotherOfAllLayouts>(dbId, dbRef, getter);
+
+                        if (m == null)
+                        {
+                            return null;
+                        }
+
+                        return m.GetObjectNo();
+                    };
+                default: throw new NotImplementedException("The data type [" + dataTypeName + "] is not supported.");
+            }
         }
     }
 }
--- src/StarDump/Model/UnloadRow.cs
+++ src/StarDump/Model/UnloadRow.cs
@@ -8,8 +8,8 @@ namespace StarDump
 {
     public class UnloadRow : Dictionary<string, object>, Starcounter.Abstractions.Database.IDbProxy
     {
-        public const string DbIdKey = "__dbId";
-        public const string DbRefKey = "__dbRef";
+        public ulong DbObjectIdentity { get; protected set; }
+        public ulong DbObjectReference { get; protected set; }
 
         public static void RegisterDatabaseType()
         {
@@ -21,22 +21,15 @@ namespace StarDump
 
         public UnloadRow(ulong dbId, ulong dbRef)
         {
-            this[DbIdKey] = dbId;
-            this[DbRefKey] = dbRef;
+            this.DbObjectIdentity = dbId;
+            this.DbObjectReference = dbRef;
         }
 
         public void Fill(UnloadTable table)
         {
-            ulong dbId = this.DbGetIdentity();
-            ulong dbRef = this.DbGetReference();
-            ulong dbHandle = Starcounter.Database.Transaction.Current.DatabaseContext.Handle;
-
             foreach (var c in table.Columns)
             {
-                ulong getter = table.CrudHelper.GetGetter(c.Name);
-                object value = this.GetValue(dbId, dbRef, getter, c.DataTypeName, c.Nullable);
-                
-                this[c.Name] = value;
+                this[c.Name] = table.CrudHelper.GetValue(this.DbObjectIdentity, this.DbObjectReference, c.Name);
             }
         }
 
@@ -63,60 +56,71 @@ namespace StarDump
             }
         }
 
-        protected object GetValue(ulong dbId, ulong dbRef, ulong getter, string dataTypeName, bool nullable)
-        {
-            if (nullable)
-            {
-                dataTypeName += "?";
-            }
+        // protected object GetValue(ulong dbId, ulong dbRef, ulong getter, string dataTypeName, bool nullable)
+        // {
+        //     if (nullable)
+        //     {
+        //         switch (dataTypeName)
+        //         {
+        //             case "string": return DbCrud.GetString(dbId, dbRef, getter);
+        //             case "byte[]": return DbCrud.GetBinary(dbId, dbRef, getter);
+        //             case "bool": return DbCrud.GetNullableBool(dbId, dbRef, getter);
+        //             case "byte": return DbCrud.GetNullableByte(dbId, dbRef, getter);
+        //             case "char": return DbCrud.GetNullableChar(dbId, dbRef, getter);
+        //             case "DateTime": return DbCrud.GetNullableDateTime(dbId, dbRef, getter);
+        //             case "decimal": return DbCrud.GetNullableDecimal(dbId, dbRef, getter);
+        //             case "double": return DbCrud.GetNullableDouble(dbId, dbRef, getter);
+        //             case "float": return DbCrud.GetNullableFloat(dbId, dbRef, getter);
+        //             case "int": return DbCrud.GetNullableInt(dbId, dbRef, getter);
+        //             case "long": return DbCrud.GetNullableLong(dbId, dbRef, getter);
+        //             case "sbyte": return DbCrud.GetNullableSByte(dbId, dbRef, getter);
+        //             case "short": return DbCrud.GetNullableShort(dbId, dbRef, getter);
+        //             case "uint": return DbCrud.GetNullableUInt(dbId, dbRef, getter);
+        //             case "reference":
+        //                 var m = DbCrud.GetDb<Starcounter.Internal.Metadata.MotherOfAllLayouts>(dbId, dbRef, getter);
 
-            switch (dataTypeName)
-            {
-                case "bool": return DbCrud.GetBool(dbId, dbRef, getter);
-                case "byte": return DbCrud.GetByte(dbId, dbRef, getter);
-                case "char": return DbCrud.GetChar(dbId, dbRef, getter);
-                case "DateTime": return DbCrud.GetDateTime(dbId, dbRef, getter);
-                case "decimal": return DbCrud.GetDecimal(dbId, dbRef, getter);
-                case "double": return DbCrud.GetDouble(dbId, dbRef, getter);
-                case "float": return DbCrud.GetFloat(dbId, dbRef, getter);
-                case "int": return DbCrud.GetInt(dbId, dbRef, getter);
-                case "long": return DbCrud.GetLong(dbId, dbRef, getter);
-                case "sbyte": return DbCrud.GetSByte(dbId, dbRef, getter);
-                case "short": return DbCrud.GetShort(dbId, dbRef, getter);
-                case "string?":
-                case "string": return DbCrud.GetString(dbId, dbRef, getter);
-                case "uint": return DbCrud.GetUInt(dbId, dbRef, getter);
-                case "ulong": return DbCrud.GetULong(dbId, dbRef, getter);
-                case "ushort": return DbCrud.GetUShort(dbId, dbRef, getter);
-                case "byte[]": return DbCrud.GetBinary(dbId, dbRef, getter);
+        //                 if (m == null)
+        //                 {
+        //                     return null;
+        //                 }
 
-                case "bool?": return DbCrud.GetNullableBool(dbId, dbRef, getter);
-                case "byte?": return DbCrud.GetNullableByte(dbId, dbRef, getter);
-                case "char?": return DbCrud.GetNullableChar(dbId, dbRef, getter);
-                case "DateTime?": return DbCrud.GetNullableDateTime(dbId, dbRef, getter);
-                case "decimal?": return DbCrud.GetNullableDecimal(dbId, dbRef, getter);
-                case "double?": return DbCrud.GetNullableDouble(dbId, dbRef, getter);
-                case "float?": return DbCrud.GetNullableFloat(dbId, dbRef, getter);
-                case "int?": return DbCrud.GetNullableInt(dbId, dbRef, getter);
-                case "long?": return DbCrud.GetNullableLong(dbId, dbRef, getter);
-                case "sbyte?": return DbCrud.GetNullableSByte(dbId, dbRef, getter);
-                case "short?": return DbCrud.GetNullableShort(dbId, dbRef, getter);
-                case "uint?": return DbCrud.GetNullableUInt(dbId, dbRef, getter);
-                case "reference":
-                case "reference?":
-                    var m = DbCrud.GetDb<Starcounter.Internal.Metadata.MotherOfAllLayouts>(dbId, dbRef, getter);
+        //                 return m.GetObjectNo();
+        //             case "ulong": return DbCrud.GetNullableULong(dbId, dbRef, getter);
+        //             case "ushort": return DbCrud.GetNullableUShort(dbId, dbRef, getter);
+        //             default: throw new NotImplementedException("The data type [" + dataTypeName + "] is not supported.");
+        //         }                
+        //     }
 
-                    if (m == null)
-                    {
-                        return null;
-                    }
+        //     switch (dataTypeName)
+        //     {
+        //         case "bool": return DbCrud.GetBool(dbId, dbRef, getter);
+        //         case "byte": return DbCrud.GetByte(dbId, dbRef, getter);
+        //         case "char": return DbCrud.GetChar(dbId, dbRef, getter);
+        //         case "DateTime": return DbCrud.GetDateTime(dbId, dbRef, getter);
+        //         case "decimal": return DbCrud.GetDecimal(dbId, dbRef, getter);
+        //         case "double": return DbCrud.GetDouble(dbId, dbRef, getter);
+        //         case "float": return DbCrud.GetFloat(dbId, dbRef, getter);
+        //         case "int": return DbCrud.GetInt(dbId, dbRef, getter);
+        //         case "long": return DbCrud.GetLong(dbId, dbRef, getter);
+        //         case "sbyte": return DbCrud.GetSByte(dbId, dbRef, getter);
+        //         case "short": return DbCrud.GetShort(dbId, dbRef, getter);
+        //         case "string": return DbCrud.GetString(dbId, dbRef, getter);
+        //         case "uint": return DbCrud.GetUInt(dbId, dbRef, getter);
+        //         case "ulong": return DbCrud.GetULong(dbId, dbRef, getter);
+        //         case "ushort": return DbCrud.GetUShort(dbId, dbRef, getter);
+        //         case "byte[]": return DbCrud.GetBinary(dbId, dbRef, getter);
+        //         case "reference":
+        //             var m = DbCrud.GetDb<Starcounter.Internal.Metadata.MotherOfAllLayouts>(dbId, dbRef, getter);
 
-                    return m.GetObjectNo();
-                case "ulong?": return DbCrud.GetNullableULong(dbId, dbRef, getter);
-                case "ushort?": return DbCrud.GetNullableUShort(dbId, dbRef, getter);
-                default: throw new NotImplementedException("The data type [" + dataTypeName + "] is not supported.");
-            }
-        }
+        //             if (m == null)
+        //             {
+        //                 return null;
+        //             }
+
+        //             return m.GetObjectNo();
+        //         default: throw new NotImplementedException("The data type [" + dataTypeName + "] is not supported.");
+        //     }
+        // }
 
         protected void SetValue(ulong dbId, ulong dbRef, ulong setter, string dataTypeName, bool nullable, object value)
         {
@@ -171,12 +175,12 @@ namespace StarDump
         #region Starcounter.Abstractions.Database.IDbProxy
         public ulong DbGetReference()
         {
-            return (ulong)this[DbRefKey];
+            return this.DbObjectReference;
         }
 
         public ulong DbGetIdentity()
         {
-            return (ulong)this[DbIdKey];
+            return this.DbObjectIdentity;
         }
         
         public void DbDelete()
@@ -186,7 +190,7 @@ namespace StarDump
 
         public bool Equals(Starcounter.Abstractions.Database.IDbProxy obj)
         {
-            return this.DbGetIdentity() == obj.DbGetIdentity();
+            return this.DbObjectIdentity == obj.DbGetIdentity();
         }
         #endregion
     }
