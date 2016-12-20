b97b7e2e4782fef033bbac3939d5c56903e0e164
ubbeK <urban.nyden@gmail.com>
2016-12-20 18:38:03 +0100

    StarDump.Core.Tests added which only contains a dummy test in order to a complete python script for TeamCity integration

---
+++ test/StarDump.Core.Tests/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace StarDump.Core.Tests
+{
+    public class DummyTest 
+    {
+        [Fact]
+        public void Test_DummyIsTrue()
+        {
+            bool isTrue = true;
+            Assert.True(isTrue);
+        }
+    }
+}
