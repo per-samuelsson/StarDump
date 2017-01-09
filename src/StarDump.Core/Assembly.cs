5e62c3da58b3d93b99890102bb10657b8d4df699
Konstantin <const.miyang@gmail.com>
2017-01-09 18:53:46 +0100

    version 0.0.1

--- src/StarDump.Core/Assembly.cs
+++ src/StarDump.Core/Assembly.cs
@@ -2,7 +2,7 @@ namespace StarDump.Core
 {
     public static class Assembly
     {
-        public const string Version = "0.0.0";
-        public const string FormatVersion = "0.0.0";
+        public const string Version = "0.0.1";
+        public const string FormatVersion = "0.0.1";
     }
 }
