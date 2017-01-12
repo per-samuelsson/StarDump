7bc0d661a6a75f2d95ff850f750e392af0a69938
ubbeK <urban.nyden@gmail.com>
2017-01-12 15:08:44 +0100

    Comments added for Version and FormatVersion

--- src/StarDump.Core/Assembly.cs
+++ src/StarDump.Core/Assembly.cs
@@ -2,7 +2,10 @@ namespace StarDump.Core
 {
     public static class Assembly
     {
+		// Version of Application
         public const string Version = "0.0.1";
+		
+		// Version of SQLite dump format
         public const string FormatVersion = "0.0.1";
     }
 }
