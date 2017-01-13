230f8baea33695afaa5ac2309abf205bdfbc948d
Konstantin <const.miyang@gmail.com>
2017-01-13 14:08:39 +0100

    Update comments
    cc @ubbeK

--- src/StarDump.Core/Assembly.cs
+++ src/StarDump.Core/Assembly.cs
@@ -2,10 +2,14 @@ namespace StarDump.Core
 {
     public static class Assembly
     {
-		// Version of Application
+        /// <summary>
+        /// Version of Application
+        /// </summary>
         public const string Version = "0.0.1";
 		
-		// Version of SQLite dump format
+        /// <summary>
+        /// Version of SQLite dump format
+        /// </summary>
         public const string FormatVersion = "0.0.1";
     }
 }
