7dda111c2d6e7c1339aea3562fec18d280e3a522
Konstantin <const.miyang@gmail.com>
2017-01-27 16:58:38 +0100

    Add demo UnloadTest with some database intercations

---
+++ test/StarDump.Core.Tests/UnloadTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Starcounter.Core;
+
+namespace StarDump.Core.Tests
+{
+    [Database]
+    public class Parent
+    {
+        public virtual string Name { get; set; }
+    }
+
+    public class UnloadTest
+    {
+        [Fact]
+        public void Test_Unload()
+        {
+            string[] args = new string[] { };
+            var host = new AppHostBuilder().AddCommandLine(args).Build();
+            
+            host.Start();
+
+            Db.Transact(() =>
+            {
+                Parent p = Db.Insert<Parent>();
+                p.Name = "Xunit test";
+                //new Parent() { Name = "Xunit test" };
+
+                p = Db.SQL<Parent>("SELECT p FROM StarDump.Core.Tests.Parent p").FirstOrDefault();
+
+                Assert.Equal(p.Name, "Xunit test");
+            });
+
+            host.Dispose();
+        }
+    }
+}
