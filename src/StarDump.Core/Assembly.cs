4ab34e6b0673b4bfad86a834ca3e9fb187e65861
Konstantin <const.miyang@gmail.com>
2017-03-16 19:06:00 +0100

    version 0.1.2

--- src/StarDump.Core/Assembly.cs
+++ src/StarDump.Core/Assembly.cs
@@ -5,7 +5,7 @@ namespace StarDump.Core
         /// <summary>
         /// Version of Application
         /// </summary>
-        public const string Version = "0.1.1";
+        public const string Version = "0.1.2";
 		
         /// <summary>
         /// Version of SQLite dump format
